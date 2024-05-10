8df10c41a120dd3ef3bab22e76c10a6f3db5605f
masudrana25 <mdmasudrana2512000@gmail.com>
2024-05-10 14:15:45 +0600

    morgna install and use

--- Back-End/src/server.js
+++ Back-End/src/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 
+// use morgan for know the user search wrong web address
+const morgan = require('morgan')
+app.use(morgan('dev'))
+
 app.get('/',(req,res)=>{
     res.send('Welcome to Server Home Page')
 })
 
+// server running port , success and error message show
 app.listen(3001, ()=>{
     console.log('server runnig in http://localhost:3001');
 })
